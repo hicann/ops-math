38efb0dac9a795a9d63f79c78433fcfa2442b9fe
gcw_SUaZx3UQ <zhangqijia1@h-partners.com>
2025-09-29 19:34:10 +0800

    [UT]:增加pows，rfft1d等算子的UT
    Co-authored-by: zhangqijia1<zhangqijia1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge add_ut into master
    
    [UT]:增加pows，rfft1d等算子的UT
    
    Created-by: gcw_SUaZx3UQ
    Commit-by: zhangqijia1
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!34

--- math/hans_decode/tests/ut/op_host/test_hans_decode_tiling.cpp
+++ math/hans_decode/tests/ut/op_host/test_hans_decode_tiling.cpp
@@ -9,59 +9,16 @@
  */
 
 #include <iostream>
-#include <vector>
-#include <nlohmann/json.hpp>
+#include <memory>
 #include <gtest/gtest.h>
-#include "register/op_tiling_registry.h"
-#include "common/utils/ut_op_util.h"
-#include "op_tiling/op_tiling_util.h"
-#include "test_common.h"
-#include "comp_ops.h"
-#include "../../../../math/hans_decode/op_host/hans_decode_tiling.h"
-#include "test_cube_util.h"
-#include "kernel_run_context_facker.h"
-#include "register/op_impl_registry_base.h"
+#include "tiling_context_faker.h"
+#include "tiling_case_executor.h"
+#include "../../../op_host/hans_decode_tiling.h"
 
 using namespace std;
 using namespace ge;
 
-struct HansDecodeTilingTestParam {
-  string caseName;
-  string opType;
-
-  // mantissa
-  ge::Format mantissaFormat;
-  ge::Format mantissaOrgFormat;
-  ge::DataType mantissaDtype;
-  std::initializer_list<int64_t> mantissaShape;
-  // pdf
-  ge::Format pdfFormat;
-  ge::Format pdfOrgFormat;
-  ge::DataType pdfDtype;
-  std::initializer_list<int64_t> pdfShape;
-  // fixed
-  ge::Format fixedFormat;
-  ge::Format fixedOrgFormat;
-  ge::DataType fixedDtype;
-  std::initializer_list<int64_t> fixedShape;
-  // var
-  ge::Format varFormat;
-  ge::Format varOrgFormat;
-  ge::DataType varDtype;
-  std::initializer_list<int64_t> varShape;
-  // recover
-  ge::Format recoverFormat;
-  ge::Format recoverOrgFormat;
-  ge::DataType recoverDtype;
-  std::initializer_list<int64_t> recoverShape;
-
-  bool reshuff;
-  uint32_t tilingKey;
-};
-
-struct HansDecodeCompileInfo {};
-
-class HansDecodeTilingRuntime : public testing::TestWithParam<HansDecodeTilingTestParam> {
+class HansDecodeTiling : public testing::Test {
  protected:
   static void SetUpTestCase() {
     std::cout << "HansDecode SetUp" << std::endl;
@@ -72,153 +29,32 @@ class HansDecodeTilingRuntime : public testing::TestWithParam<HansDecodeTilingTe
   }
 };
 
-TEST_P(HansDecodeTilingRuntime, generalCases) {
-  HansDecodeTilingTestParam param = GetParam();
-
-  std::string op_type("HansDecode");
-  ASSERT_NE(gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str()), nullptr);
-  auto tiling_func = gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str())->tiling;
-  auto tiling_parse_func = gert::OpImplRegistry::GetInstance().GetOpImpl(op_type.c_str())->tiling_parse;
-
-  string compile_info_string = R"({
-      "hardware_info": {"BT_SIZE": 0, "load3d_constraints": "1",
-                        "Intrinsic_fix_pipe_l0c2out": false, "Intrinsic_data_move_l12ub": true, "Intrinsic_data_move_l0c2ub": true, "Intrinsic_data_move_out2l1_nd2nz": false,
-                        "UB_SIZE": 196608, "L2_SIZE": 33554432, "L1_SIZE": 524288,
-                        "L0A_SIZE": 65536, "L0B_SIZE": 65536, "L0C_SIZE": 131072,
-                        "CORE_NUM": 40}
-                        })";
-  map<string, string> soc_infos;
-  map<string, string> aicore_spec;
-  map<string, string> intrinsics;
-  GetPlatFormInfos(compile_info_string.c_str(), soc_infos, aicore_spec, intrinsics);
-
-  // platform info
-  fe::PlatFormInfos platform_info;
-  platform_info.Init();
-  // compile info
-  HansDecodeCompileInfo compile_info;
-  // tilingParseFunc simulate
-  auto kernel_holder =
-      gert::KernelRunContextFaker()
-          .KernelIONum(2, 1)
-          .Inputs({const_cast<char*>(compile_info_string.c_str()), reinterpret_cast<void*>(&platform_info)})
-          .Outputs({&compile_info})
-          .Build();
-  ASSERT_TRUE(kernel_holder.GetContext<gert::TilingParseContext>()->GetPlatformInfo()->Init());
-  kernel_holder.GetContext<gert::TilingParseContext>()->GetPlatformInfo()->SetPlatformRes("SoCInfo", soc_infos);
-  kernel_holder.GetContext<gert::TilingParseContext>()->GetPlatformInfo()->SetPlatformRes("AICoreSpec", aicore_spec);
-  kernel_holder.GetContext<gert::TilingParseContext>()->GetPlatformInfo()->SetCoreNumByCoreType("AICore");
-  kernel_holder.GetContext<gert::TilingParseContext>()->GetPlatformInfo()->SetPlatformRes("AICoreintrinsicDtypeMap",
-                                                                                          intrinsics);
-  ASSERT_EQ(tiling_parse_func(kernel_holder.GetContext<gert::KernelContext>()), ge::GRAPH_SUCCESS);
-
-  // tilingFunc simulate
-  auto tiling_data = gert::TilingData::CreateCap(4096);
-  ASSERT_NE(tiling_data, nullptr);
-  auto workspace_size_holer = gert::ContinuousVector::Create<size_t>(4096);
-  auto ws_size = reinterpret_cast<gert::ContinuousVector*>(workspace_size_holer.get());
-  gert::StorageShape mantissaShape = {param.mantissaShape, param.mantissaShape};
-  gert::StorageShape fixedShape = {param.fixedShape, param.fixedShape};
-  gert::StorageShape varShape = {param.varShape, param.varShape};
-  gert::StorageShape pdfShape = {param.pdfShape, param.pdfShape};
-  gert::StorageShape recoverShape = {param.recoverShape, param.recoverShape};
-
-  auto holder = gert::TilingContextFaker()
-                    .NodeIoNum(4, 1)
-                    .IrInstanceNum({1, 1, 1, 1})
-                    .InputShapes({&mantissaShape, &fixedShape, &varShape, &pdfShape})
-                    .OutputShapes({&recoverShape})
-                    .NodeInputTd(0, param.mantissaDtype, param.mantissaOrgFormat, param.mantissaFormat)
-                    .NodeInputTd(1, param.fixedDtype, param.fixedOrgFormat, param.fixedFormat)
-                    .NodeInputTd(2, param.varDtype, param.varOrgFormat, param.varFormat)
-                    .NodeInputTd(3, param.pdfDtype, param.pdfOrgFormat, param.pdfFormat)
-                    .NodeOutputTd(0, param.recoverDtype, param.recoverOrgFormat, param.recoverFormat)
-                    .NodeAttrs({{"reshuff", ge::AnyValue::CreateFrom<bool>(param.reshuff)}})
-                    .PlatformInfo(reinterpret_cast<char*>(&platform_info))
-                    .TilingData(tiling_data.get())
-                    .Workspace(ws_size)
-                    .Build();
-
-  auto tiling_context = holder.GetContext<gert::TilingContext>();
-  ASSERT_NE(tiling_context->GetPlatformInfo(), nullptr);
-  holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("SoCInfo", soc_infos);
-  holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("AICoreSpec", aicore_spec);
-  holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetCoreNumByCoreType("AICore");
-  holder.GetContext<gert::TilingContext>()->GetPlatformInfo()->SetPlatformRes("AICoreintrinsicDtypeMap", intrinsics);
-  std::cout << "[ATTENTION] varShape: " << param.varShape.begin() << std::endl;
-
-  // workspaces nullptr return failed
-  EXPECT_EQ(tiling_func(tiling_context), ge::GRAPH_SUCCESS);
-  // todo check tiling result
-  auto tiling_key = tiling_context->GetTilingKey();
-  ASSERT_EQ(tiling_key, param.tilingKey);
-}
-
-inline int64_t GetProcessBlockDim(int64_t size, int64_t aivNum) {
-  int64_t properAivNum = size / 32768;
-  return properAivNum > aivNum ? aivNum : properAivNum;
-}
-
-inline int64_t getFixedNumelByRatio(int64_t expSize, float ratio, ge::DataType dtype) {
-  return int64_t(expSize * ratio) / GetSizeByDataType(dtype);
-}
-
-inline int64_t getVarNumelByRatio(int64_t expSize, float ratio, ge::DataType dtype) {
-  int64_t fixedSize = getFixedNumelByRatio(expSize, ratio, dtype) * GetSizeByDataType(dtype);
-  int64_t compressUpperBoundBytes = expSize + expSize / 64 + 8448 * GetProcessBlockDim(expSize, 40) + 512;
-  return (compressUpperBoundBytes - fixedSize) / GetSizeByDataType(dtype) + 1;
-}
-
-static HansDecodeTilingTestParam testParamCases[] = {
-    // case 1
-    {"HansDecode_Basic_Test01",
-     "HansDecode",
-     ge::FORMAT_ND,
-     ge::FORMAT_ND,
-     ge::DT_FLOAT,
-     {1, 4096 * 4096 * 3 / 4},
-     ge::FORMAT_ND,
-     ge::FORMAT_ND,
-     ge::DT_INT32,
-     {1, 256},
-     ge::FORMAT_ND,
-     ge::FORMAT_ND,
-     ge::DT_FLOAT,
-     {1, getFixedNumelByRatio(4096 * 4096, 0.2, ge::DT_FLOAT)},
-     ge::FORMAT_ND,
-     ge::FORMAT_ND,
-     ge::DT_FLOAT,
-     {1, getVarNumelByRatio(4096 * 4096, 0.2, ge::DT_FLOAT)},
-     ge::FORMAT_ND,
-     ge::FORMAT_ND,
-     ge::DT_FLOAT,
-     {4096, 4096},
-     false,
-     4},  // case 1
-    {"HansDecode_Basic_Test02",
-     "HansDecode",
-     ge::FORMAT_ND,
-     ge::FORMAT_ND,
-     ge::DT_FLOAT16,
-     {1, 4096 * 4096 / 2},
-     ge::FORMAT_ND,
-     ge::FORMAT_ND,
-     ge::DT_INT32,
-     {1, 256},
-     ge::FORMAT_ND,
-     ge::FORMAT_ND,
-     ge::DT_FLOAT16,
-     {1, getFixedNumelByRatio(4096 * 4096, 0.2, ge::DT_FLOAT16)},
-     ge::FORMAT_ND,
-     ge::FORMAT_ND,
-     ge::DT_FLOAT16,
-     {1, getVarNumelByRatio(4096 * 4096, 0.2, ge::DT_FLOAT16)},
-     ge::FORMAT_ND,
-     ge::FORMAT_ND,
-     ge::DT_FLOAT16,
-     {4096, 4096},
-     false,
-     2},
+struct HansDecodeCompileInfo {
+    uint32_t coreNum = 0;
+    uint64_t ubSizePlatForm = 0;
 };
 
-INSTANTIATE_TEST_CASE_P(HansDecode, HansDecodeTilingRuntime, testing::ValuesIn(testParamCases));
+TEST_F(HansDecodeTiling, ascend910B1_test_tiling_001)
+{
+    HansDecodeCompileInfo compileInfo = {48, 196608};
+    gert::TilingContextPara tilingContextPara(
+        "HansDecode",
+	      {
+            {{{49152}, {49152}}, ge::DT_FLOAT, ge::FORMAT_ND},
+            {{{16384}, {16384}}, ge::DT_FLOAT, ge::FORMAT_ND},
+            {{{16384}, {16384}}, ge::DT_FLOAT, ge::FORMAT_ND},
+            {{{256}, {256}}, ge::DT_INT32, ge::FORMAT_ND}
+        },
+        {
+            {{{65536}, {65536}}, ge::DT_FLOAT, ge::FORMAT_ND},
+        },
+	      {
+          gert::TilingContextPara::OpAttr("reshuff", Ops::Math::AnyValue::CreateFrom<bool>(false))
+        },
+         &compileInfo);
+    uint64_t expectTilingKey = 4;
+    string expectTilingData = "196608 65536 262144 1048576 0 ";
+    std::vector<size_t> expectWorkspaces = {16777216};
+    ExecuteTestCase(tilingContextPara, ge::GRAPH_SUCCESS, expectTilingKey, expectTilingData, expectWorkspaces);
+}
+
