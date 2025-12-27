e765389c9617efffaa71335afdc8e0ec417c206d
Hou_jialin <houjialin3@huawei.com>
2025-12-27 11:28:51 +0800

    fix right_shift l0 api bug
    Co-authored-by: Hou_jialin<houjialin3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !514 merge br_fix_rightshift into master
    
    fix right_shift l0 api bug
    
    Created-by: Hou_jialin
    Commit-by: Hou_jialin
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!514

--- math/right_shift/op_api/right_shift.cpp
+++ math/right_shift/op_api/right_shift.cpp
@@ -25,37 +25,54 @@ namespace l0op {
 OP_TYPE_REGISTER(RightShift);
 
 static const std::initializer_list<op::DataType> DTYPE_SUPPORT_LIST = {
-    op::DataType::DT_INT8, op::DataType::DT_INT16,
-    op::DataType::DT_INT32, op::DataType::DT_INT64,
-    op::DataType::DT_UINT8, op::DataType::DT_UINT16,
-    op::DataType::DT_UINT32, op::DataType::DT_UINT64};
- 
+   op::DataType::DT_INT8, op::DataType::DT_INT16,
+   op::DataType::DT_INT32, op::DataType::DT_INT64,
+   op::DataType::DT_UINT8, op::DataType::DT_UINT16,
+   op::DataType::DT_UINT32, op::DataType::DT_UINT64};
+
 bool IsSupported(const aclTensor* x, const aclTensor* y) {
-    if (!op::CheckType(x->GetDataType(), DTYPE_SUPPORT_LIST)) {
-        return false;
-    }
-    if (!op::CheckType(y->GetDataType(), DTYPE_SUPPORT_LIST)) {
-        return false;
-    }
-    return true;
+   if (!op::CheckType(x->GetDataType(), DTYPE_SUPPORT_LIST)) {
+       return false;
+   }
+   if (!op::CheckType(y->GetDataType(), DTYPE_SUPPORT_LIST)) {
+       return false;
+   }
+   return true;
 }
 
+const aclTensor* RightShiftAiCore(const aclTensor* x, const aclTensor* y, const aclTensor* z, aclOpExecutor* executor) {
+   L0_DFX(RightShiftAiCore, x, y, z);
+
+   auto ret = ADD_TO_LAUNCHER_LIST_AICORE(RightShift, OP_INPUT(x, y), OP_OUTPUT(z));
+   OP_CHECK(
+       ret == ACLNN_SUCCESS, OP_LOGE(ACLNN_ERR_INNER_NULLPTR, "RightShiftAiCore ADD_TO_LAUNCHER_LIST_AICORE failed."),
+       return nullptr);
+   return z;
+}
+
+const aclTensor* RightShiftAiCpu(const aclTensor* x, const aclTensor* y, const aclTensor* z, aclOpExecutor* executor) {
+   L0_DFX(RightShiftAiCpu, x, y, z);
+
+   static internal::AicpuTaskSpace space("RightShift");
+   auto ret = ADD_TO_LAUNCHER_LIST_AICPU(RightShift, OP_ATTR_NAMES(), OP_INPUT(x, y), OP_OUTPUT(z));
+   OP_CHECK(
+       ret == ACLNN_SUCCESS, OP_LOGE(ACLNN_ERR_INNER_NULLPTR, "RightShiftAiCpu ADD_TO_LAUNCHER_LIST_AICPU failed."),
+       return nullptr);
+   return z;
+}
 
 const aclTensor* RightShift(const aclTensor* x, const aclTensor* y, aclOpExecutor* executor) {
-    auto z = executor->AllocTensor(x->GetViewShape(), x->GetDataType());
-    CHECK_RET(z != nullptr, nullptr);
-    if (!IsSupported(x, y)) {
-        OP_LOGE(ACLNN_ERR_PARAM_INVALID, "The input dtype for rightshift is not supported。");
-        return nullptr;
-    }
-
-    L0_DFX(RightShift, x, y, z);
-
-    static internal::AicpuTaskSpace space("RightShift");
-    ADD_TO_LAUNCHER_LIST_AICPU(RightShift, OP_ATTR_NAMES(),
-                               OP_INPUT(x, y),
-                               OP_OUTPUT(z));
-    return z;
+   auto z = executor->AllocTensor(x->GetViewShape(), x->GetDataType());
+   CHECK_RET(z != nullptr, nullptr);
+   if (!IsSupported(x, y)) {
+       OP_LOGE(ACLNN_ERR_PARAM_INVALID, "The input dtype for rightshift is not supported.");
+       return nullptr;
+   }
+   auto socVersion = GetCurrentPlatformInfo().GetSocVersion();
+   if (socVersion != SocVersion::ASCEND910_95) {
+       return RightShiftAiCpu(x, y, z, executor);
+   } else {
+       return RightShiftAiCore(x, y, z, executor);
+   }
 }
- 
 }  // namespace l0op
