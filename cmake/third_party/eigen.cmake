2008d462c40721d6747b0c3db415fe23d6d2b22e
cann-robot <songchangxia@cann.team>
2025-12-28 18:22:48 +0800

    修复eigen三方库下载失败的问题
    Co-authored-by: slx2008<shaolixin@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !543 merge master into master
    
    修复eigen三方库下载失败的问题
    
    Created-by: songkai111
    Commit-by: slx2008
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!543

--- cmake/third_party/eigen.cmake
+++ cmake/third_party/eigen.cmake
@@ -11,32 +11,52 @@ if(POLICY CMP0135)
     cmake_policy(SET CMP0135 NEW)
 endif()
 set(EIGEN_VERSION_PKG eigen-3.4.0.tar.gz)
-if(IS_DIRECTORY "${CANN_3RD_LIB_PATH}/eigen")	
-  set(REQ_URL "${CANN_3RD_LIB_PATH}/eigen")
+
+unset(eigen_FOUND CACHE)
+unset(EIGEN_INCLUDE CACHE)
+
+find_path(EIGEN_INCLUDE
+        NAMES Eigen/Eigen
+        NO_CMAKE_SYSTEM_PATH
+        NO_CMAKE_FIND_ROOT_PATH
+        PATHS ${CANN_3RD_LIB_PATH}/eigen)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(eigen
+        FOUND_VAR
+        eigen_FOUND
+        REQUIRED_VARS
+        EIGEN_INCLUDE
+        )
+
+if(eigen_FOUND)
+  message(STATUS "Found eigen in ${CANN_3RD_LIB_PATH}/eigen")
 else()
-  set(REQ_URL "https://gitcode.com/cann-src-third-party/eigen/releases/download/3.4.0/${EIGEN_VERSION_PKG}")
+  if(EXISTS "${CANN_3RD_LIB_PATH}/pkg/${EIGEN_VERSION_PKG}")
+    set(REQ_URL "file://${CANN_3RD_LIB_PATH}/pkg/${EIGEN_VERSION_PKG}")
+  else()
+    set(REQ_URL "https://gitcode.com/cann-src-third-party/eigen/releases/download/3.4.0/${EIGEN_VERSION_PKG}")
+  endif()
+
+  include(ExternalProject)
+  ExternalProject_Add(external_eigen_math
+    URL               ${REQ_URL}
+    DOWNLOAD_DIR      ${CANN_3RD_LIB_PATH}/pkg
+    SOURCE_DIR        ${CANN_3RD_LIB_PATH}/eigen
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND     ""
+    INSTALL_COMMAND   ""
+  )
+
+  ExternalProject_Get_Property(external_eigen_math SOURCE_DIR)
+  set(EIGEN_INCLUDE ${SOURCE_DIR})
 endif()
-
-set(EIGEN_DOWNLOAD_PATH ${CANN_3RD_LIB_PATH}/pkg)
-
-include(ExternalProject)
-ExternalProject_Add(external_eigen_math
-  URL               ${REQ_URL}
-  DOWNLOAD_DIR      ${EIGEN_DOWNLOAD_PATH}
-  DOWNLOAD_NO_EXTRACT 0   #自动解压
-  PREFIX            third_party
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND     ""
-  INSTALL_COMMAND   ""
-)
-
-ExternalProject_Get_Property(external_eigen_math SOURCE_DIR)
 
 add_library(EigenMath INTERFACE)
 target_compile_options(EigenMath INTERFACE -w)
 
 set_target_properties(EigenMath PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${SOURCE_DIR}"
+  INTERFACE_INCLUDE_DIRECTORIES "${EIGEN_INCLUDE}"
 )
 add_dependencies(EigenMath external_eigen_math)
 
