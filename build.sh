abeaef1ee3e1c57e3bf53da9d9c04a7384bc6ac0
pingchuantang <tangpingchuan@huawei.com>
2026-01-17 11:43:59 +0800

    fix COMPILED_OPS bug
    Co-authored-by: tangpingchuan<tangpingchuan@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !784 merge remove_reop into master
    
    fix COMPILED_OPS bug
    
    Created-by: pingchuantang
    Commit-by: tangpingchuan
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!784

--- build.sh
+++ build.sh
@@ -916,7 +916,6 @@ parse_changed_files() {
 
   COMPILED_OPS=$(python3 scripts/ci/parse_changed_ops.py $CHANGED_FILES "$ENABLE_EXPERIMENTAL")
   echo "related ops "$COMPILED_OPS
-  COMPILED_OPS='log'
 
   if [[ -z $COMPILED_OPS ]]; then
     if [[ "$ENABLE_EXPERIMENTAL" == "TRUE" ]]; then
