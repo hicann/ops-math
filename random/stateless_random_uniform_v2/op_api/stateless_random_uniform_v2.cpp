d791309a582309af6e4797c2e3644e137fe4ab38
zhangdongdong34 <zhangdongdong34@huawei.com>
2026-02-12 17:57:48 +0800

    StatelessRandomUniformV2算子支持AICPU
    Co-authored-by: zhangdongdong34<zhangdongdong34@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1178 merge random_uniform_v2 into master
    
    StatelessRandomUniformV2算子支持AICPU
    
    Created-by: zhangdongdong34
    Commit-by: zhangdongdong34
    Merged-by: cann-robot
    Description: ## 描述
    1. 该算子在一些老的芯片版本不支持Aicore实现，通过AICPU补齐该算子支持能力
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-math/issues/680
    
    ## 测试
    二级冒烟、白盒测试
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!1178

--- random/stateless_random_uniform_v2/op_api/stateless_random_uniform_v2.cpp
+++ random/stateless_random_uniform_v2/op_api/stateless_random_uniform_v2.cpp
@@ -14,6 +14,7 @@
  */
 
 #include "stateless_random_uniform_v2.h"
+#include "op_api/aclnn_check.h"
 #include "opdev/aicpu/aicpu_task.h"
 #include "opdev/data_type_utils.h"
 #include "opdev/format_utils.h"
@@ -29,6 +30,21 @@ namespace l0op {
 
 OP_TYPE_REGISTER(StatelessRandomUniformV2);
 static const int64_t OFFSET_LIST_SIZE = 2;
+
+// AICPU算子kernel 
+static const aclTensor* StatelessRandomUniformV2AiCpu( 
+    const aclTensor* inputSize, const aclTensor* seed, const aclTensor* offset, const aclTensor* alg, aclTensor* out, 
+    aclOpExecutor* executor) 
+{ 
+    L0_DFX(StatelessRandomUniformV2AiCpu, inputSize, seed, offset, alg); 
+
+    static internal::AicpuTaskSpace space("StatelessRandomUniformV2", ge::DEPEND_CONST_VALUE, true); 
+    auto ret = ADD_TO_LAUNCHER_LIST_AICPU( 
+        StatelessRandomUniformV2, OP_ATTR_NAMES({"dtype", "Tshape"}), OP_INPUT(inputSize, seed, offset, alg), 
+        OP_OUTPUT(out), OP_ATTR(out->GetDataType(), inputSize->GetDataType())); 
+    CHECK_RET(ret == ACLNN_SUCCESS, nullptr); 
+    return out; 
+}
 
 // AICore算子kernel
 static const aclTensor* StatelessRandomUniformV2AiCore(
@@ -55,12 +71,21 @@ const aclTensor* StatelessRandomUniformV2(
     aclIntArray* offsetList = executor->AllocIntArray(offsetVector.data(), OFFSET_LIST_SIZE);
     auto offsetTensor = executor->ConvertToTensor(offsetList, op::DataType::DT_UINT64);
     auto algTensor = executor->ConvertToTensor(executor->AllocScalar(alg), op::DataType::DT_INT32);
-    aclTensor* out;
-    if (self->GetDataType() == op::DataType::DT_FLOAT16) {
+    aclTensor* out = nullptr;
+    if (self->GetDataType() == op::DataType::DT_FLOAT16)
+    {
         out = executor->AllocTensor(self->GetViewShape(), op::DataType::DT_FLOAT16, self->GetViewFormat());
     } else {
         out = executor->AllocTensor(self->GetViewShape(), op::DataType::DT_FLOAT, self->GetViewFormat());
     }
-    return StatelessRandomUniformV2AiCore(inputSize, seedTensor, offsetTensor, algTensor, out, executor);
+
+    // 根据芯片型号，判断AICore 是否支持 
+    if (IsRegBase())
+    {
+        return StatelessRandomUniformV2AiCore(inputSize, seedTensor, offsetTensor, algTensor, out, executor);
+    } else {
+        return StatelessRandomUniformV2AiCpu(inputSize, seedTensor, offsetTensor, algTensor, out, executor);
+    }
+    
 }
 } // namespace l0op
