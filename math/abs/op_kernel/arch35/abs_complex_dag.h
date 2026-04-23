806a577c3a09ba46a79e672efcb67459e101b70b
qq_52874943 <gaomingyuan3@h-partners.com>
2026-04-23 10:34:14 +0800

    调整ABS算子Kernel实现，使其精度与标杆保持一致
    Co-authored-by: qq_52874943<gaomingyuan3@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1990 merge master into master
    
    调整ABS算子Kernel实现，使其精度与标杆保持一致
    
    Created-by: qq_52874943
    Commit-by: qq_52874943
    Merged-by: cann-robot
    Description: ## 描述
    调整ABS算子Kernel实现，使其精度与标杆保持一致
    
    ## 关联的Issue
    关联Issue[#1336](https://gitcode.com/cann/ops-math/issues/1336)
    
    
    ## 测试
    本地已完成新精度对比与性能测试，通过静态检查、蓝区冒烟、算子ST以及算子二级冒烟。
    
    ## 文档更新
    修正文档中算子命名不规范问题，打开Ascend950支持选项。
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!1990

--- math/abs/op_kernel/arch35/abs_complex_dag.h
+++ math/abs/op_kernel/arch35/abs_complex_dag.h
@@ -31,24 +31,129 @@ struct AbscomplexCustom : public Vec::ElemwiseUnaryOP<T, U> {
     __aicore__ inline AbscomplexCustom(LocalTensor<T>& dst, LocalTensor<U>& src, uint32_t count)
     {
 #ifdef __CCE_AICORE__
-        MicroAPI::RegTensor<U, MicroAPI::RegTraitNumTwo> vSrcReg0;
-        MicroAPI::RegTensor<T, MicroAPI::RegTraitNumOne> vDstReg0;
+
+        // 常量定义 - float精度
+        constexpr uint32_t MIN_NORMAL_FLOAT = 0x00800000u;
+        constexpr float SCALE_UP = 16777216.0f;
+        constexpr float SCALE_DOWN = 5.9604644775390625e-8f;
+        constexpr int16_t SHR_NUM_FOR_FP32 = 23;
+        constexpr uint32_t EXP_BIAS = 0x3F800000u;      // 1.0的浮点表示(指数127，尾数0)
+
+        using calcTypeInt = typename std::conditional<AscendC::Std::IsSame<T, half>::value, uint16_t, uint32_t>::type;
+
+        // 寄存器定义
+        MicroAPI::RegTensor<T> vSrcReg0;
+        MicroAPI::RegTensor<T> vSrcReg1;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vA;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vB;
+        MicroAPI::RegTensor<uint32_t, MicroAPI::RegTraitNumOne> vA0;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vA1;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vB1;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vAS;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vBS;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vA1Sq;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vB1Sq;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vSumSq;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vS;
+        MicroAPI::RegTensor<T, MicroAPI::RegTraitNumOne> vResult;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vResultS;
+
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vAbsSrcReg0;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vAbsSrcReg1;
+
+        MicroAPI::RegTensor<uint32_t, MicroAPI::RegTraitNumOne> vMinNormalFloat;
+        MicroAPI::RegTensor<uint32_t, MicroAPI::RegTraitNumOne> vExpBias;
+        MicroAPI::RegTensor<uint32_t, MicroAPI::RegTraitNumOne> vZero;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vSCALE_UP;
+        MicroAPI::RegTensor<float, MicroAPI::RegTraitNumOne> vSCALE_DOWN;
+
         uint32_t sreg = (uint32_t)count;
         MicroAPI::MaskReg preg;
-        static constexpr uint32_t repeatStride =
-            static_cast<uint32_t>(VECTOR_REG_WIDTH / sizeof(U) * MicroAPI::RegTraitNumTwo.REG_NUM);
+        MicroAPI::MaskReg preg2;
+        MicroAPI::MaskReg preg3;
+        MicroAPI::MaskReg preg4;
+        constexpr uint32_t vflen = AscendC::VECTOR_REG_WIDTH / sizeof(float);
+        constexpr uint32_t ub = AscendC::VECTOR_REG_WIDTH / AscendC::ONE_BLOCK_SIZE;
+
+        static constexpr uint32_t repeatStride = static_cast<uint32_t>(VECTOR_REG_WIDTH / sizeof(U));
         uint16_t repeatTime = static_cast<uint16_t>(CeilDivision(count, repeatStride));
-        __ubuf__ U* srcAddr = (__ubuf__ U*)(src.GetPhyAddr());
+        __ubuf__ T* srcAddr = (__ubuf__ T*)(src.GetPhyAddr());
         __ubuf__ T* dstAddr = (__ubuf__ T*)(dst.GetPhyAddr());
         __VEC_SCOPE__
         {
-            for (uint16_t i = 0; i < repeatTime; ++i) {
-                preg = MicroAPI::UpdateMask<U, MicroAPI::RegTraitNumTwo>(sreg);
-                MicroAPI::LoadAlign(vSrcReg0, srcAddr + i * repeatStride);
-                MicroAPI::Abs<
-                    T, U, MicroAPI::MaskMergeMode::ZEROING, MicroAPI::RegTensor<T, MicroAPI::RegTraitNumOne>,
-                    MicroAPI::RegTensor<U, MicroAPI::RegTraitNumTwo>>(vDstReg0, vSrcReg0, preg);
-                MicroAPI::StoreAlign(dstAddr + i * repeatStride, vDstReg0, preg);
+            // 加载常量
+            MicroAPI::Duplicate(vMinNormalFloat, MIN_NORMAL_FLOAT);
+            MicroAPI::Duplicate(vExpBias, EXP_BIAS);
+            MicroAPI::Duplicate(vSCALE_UP, SCALE_UP);
+            MicroAPI::Duplicate(vSCALE_DOWN, SCALE_DOWN);
+            MicroAPI::Duplicate(vZero, 0);
+
+            static constexpr AscendC::MicroAPI::CastTrait castTraitZero = {
+                AscendC::MicroAPI::RegLayout::ZERO, AscendC::MicroAPI::SatMode::UNKNOWN,
+                AscendC::MicroAPI::MaskMergeMode::ZEROING, RoundMode::UNKNOWN};
+            static constexpr AscendC::MicroAPI::CastTrait castTraitOne = {
+                AscendC::MicroAPI::RegLayout::ONE, AscendC::MicroAPI::SatMode::UNKNOWN,
+                AscendC::MicroAPI::MaskMergeMode::ZEROING, RoundMode::UNKNOWN};
+            static constexpr AscendC::MicroAPI::CastTrait castTrait32to16 = {
+                AscendC::MicroAPI::RegLayout::ZERO, AscendC::MicroAPI::SatMode::SAT,
+                AscendC::MicroAPI::MaskMergeMode::ZEROING, RoundMode::CAST_RINT};
+
+            for (uint16_t i = 0; i < repeatTime; i++) {
+                preg = MicroAPI::UpdateMask<float, MicroAPI::RegTraitNumOne>(sreg);
+                if constexpr (std::is_same<U, complex32>::value) {
+                    preg2 = AscendC::Reg::CreateMask<uint16_t, AscendC::Reg::MaskPattern::ALL>();
+                    Reg::LoadAlign(vSrcReg0, srcAddr + i * vflen * 2);
+                    Reg::Cast<float, T, castTraitZero>(vA, vSrcReg0, preg2);
+                    Reg::Cast<float, T, castTraitOne>(vB, vSrcReg0, preg2);
+                    Reg::Mul(vA1Sq, vA, vA, preg);
+                    Reg::Mul(vB1Sq, vB, vB, preg);
+                    Reg::Add(vSumSq, vA1Sq, vB1Sq, preg);
+                    Reg::Sqrt(vS, vSumSq, preg);
+                    Reg::Cast<T, float, castTrait32to16>(vResult, vS, preg);
+                    Reg::StoreAlign<T, Reg::StoreDist::DIST_PACK_B32>(dstAddr + i * vflen, vResult, preg);
+                } else {
+                    Reg::LoadAlign<T, Reg::LoadDist::DIST_DINTLV_B32>(vSrcReg0, vSrcReg1, srcAddr + i * vflen * 2);
+                    Reg::Abs(vAbsSrcReg0, vSrcReg0, preg);
+                    Reg::Abs(vAbsSrcReg1, vSrcReg1, preg);
+                    Reg::Compare<float, CMPMODE::GE>(preg2, vAbsSrcReg0, vAbsSrcReg1, preg);
+                    Reg::Select<float>(vA, vAbsSrcReg0, vAbsSrcReg1, preg2);
+                    Reg::Select<float>(vB, vAbsSrcReg1, vAbsSrcReg0, preg2);
+
+                    // 0的处理
+                    Reg::Compare<uint32_t, CMPMODE::EQ>(
+                        preg4, (AscendC::MicroAPI::RegTensor<uint32_t>&)vB, vZero, preg);
+
+                    // subnormal处理
+                    Reg::Compare<uint32_t, CMPMODE::LT>(
+                        preg3, (AscendC::MicroAPI::RegTensor<uint32_t>&)vA, vMinNormalFloat, preg);
+                    Reg::Mul(vAS, vA, vSCALE_UP, preg);
+                    Reg::Mul(vBS, vB, vSCALE_UP, preg);
+                    Reg::Select(vA, vAS, vA, preg3);
+                    Reg::Select(vB, vBS, vB, preg3);
+                    // 提取A的指数因子（对vAForExp操作）
+                    Reg::ShiftRights(vA0, (AscendC::Reg::RegTensor<uint32_t>&)vA, SHR_NUM_FOR_FP32, preg);
+                    Reg::ShiftLefts(vA0, vA0, SHR_NUM_FOR_FP32, preg);
+                    Reg::Sub((AscendC::Reg::RegTensor<uint32_t>&)vA1, (AscendC::Reg::RegTensor<uint32_t>&)vA, vA0, preg);
+                    Reg::Sub((AscendC::Reg::RegTensor<uint32_t>&)vB1, (AscendC::Reg::RegTensor<uint32_t>&)vB, vA0, preg);
+
+                    Reg::Add((AscendC::Reg::RegTensor<uint32_t>&)vA1, (AscendC::Reg::RegTensor<uint32_t>&)vA1, vExpBias, preg);
+                    Reg::Add((AscendC::Reg::RegTensor<uint32_t>&)vB1, (AscendC::Reg::RegTensor<uint32_t>&)vB1, vExpBias, preg);
+
+                    // 计算归化值
+                    Reg::Mul(vA1Sq, vA1, vA1, preg);
+                    Reg::Mul(vB1Sq, vB1, vB1, preg);
+                    Reg::Add(vSumSq, vA1Sq, vB1Sq, preg);
+                    Reg::Sqrt(vS, vSumSq, preg);
+
+                    Reg::Mul(vResult, vS, (AscendC::Reg::RegTensor<float>&)vA0, preg);
+                    Reg::Mul(vResultS, vResult, vSCALE_DOWN, preg);
+                    Reg::Select(vResult, vResultS, vResult, preg3);
+
+                    // 0的处理
+                    Reg::Select(vResult, vA, vResult, preg4);
+
+                    MicroAPI::StoreAlign(dstAddr + i * vflen, vResult, preg);
+                }
             }
         }
 #endif
