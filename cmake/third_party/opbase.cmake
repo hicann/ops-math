84d83e1d6c63d23a1fbd55c7c9122af235ba86fa
zl_hw <zhanglei121@huawei.com>
2026-05-25 09:21:34 +0800

    adapt new opbase submodel
    Co-authored-by: 张磊<zhanglei121@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2875 merge master into master
    
    adapt new opbase submodel
    
    Created-by: zl_hw
    Commit-by: 张磊
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!2875

--- cmake/third_party/opbase.cmake
+++ cmake/third_party/opbase.cmake
@@ -7,7 +7,7 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 # ----------------------------------------------------------------------------
-set(OPBASE_TAG_ID eee2f4c05b4b54ac76a7f7093d8d33bfde278c00)
+set(OPBASE_TAG_ID ee5334d157016d2afa736d4a23381855dbf1fe37)
 
 if(EXISTS "${PROJECT_SOURCE_DIR}/../../ops-base")
   get_filename_component(OPBASE_SOURCE_PATH
