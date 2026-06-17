8fc151f2ac0744b77a1abe3e6f5ab4ad3c3f3146
wangrui_ <wangrui124@huawei.com>
2026-06-17 15:13:58 +0800

    fix:reduce_min/reduce_max/reduce_prod算子onnx插件支持noop_with_empty_axis属性
    Co-authored-by: wangrui<wangrui124@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3371 merge master into master
    
    fix:reduce_min/reduce_max/reduce_prod算子onnx插件支持noop_with_empty_axis属性
    
    Created-by: wangrui_
    Commit-by: wangrui
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    reduce_min/reduce_max/reduce_prod算子onnx插件支持noop_with_empty_axis属性
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    https://gitcode.com/cann/ops-math/issues/1992
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!3371

--- math/reduce_max/framework/reduce_max_onnx_plugin.cpp
+++ math/reduce_max/framework/reduce_max_onnx_plugin.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -12,8 +12,25 @@
 #include "math/reduce_max/op_graph/reduce_max_proto.h"
 
 using namespace ge;
+using ge::Operator;
+
 namespace domi {
 using NodeProto = ge::onnx::NodeProto;
+
+static Status GetInputTensorDimNum(const Operator& data_op, int64_t& dim_num)
+{
+    ge::TensorDesc input_desc = data_op.GetInputDesc(0);
+    auto shape = input_desc.GetShape();
+    if (shape.GetDimNum() <= 0) {
+        OP_LOGE("GetInputTensorDimNum", "Get input shape is invalid.");
+        return FAILED;
+    }
+
+    dim_num = shape.GetDimNum();
+    OP_LOGI(GetOpName(data_op).c_str(), "GetInputTensorDimNum is: %ld", dim_num);
+    return SUCCESS;
+}
+
 static Status ParseParamsReduceMax(const Message* op_src, ge::Operator& op_dest)
 {
     const NodeProto* node = dynamic_cast<const NodeProto*>(op_src);
@@ -72,6 +89,25 @@ static Status ParseOpToGraphReduceMax(const ge::Operator& op, Graph& graph)
         OP_LOGE(GetOpName(op).c_str(), "get axes from op failed");
         return FAILED;
     }
+
+    if (axes.GetSize() == 0) {
+        int64_t input_dim_num = 0;
+        if (GetInputTensorDimNum(op, input_dim_num) != SUCCESS) {
+            OP_LOGE(GetOpName(op).c_str(), "Failed to get input tensor dimensions");
+            return FAILED;
+        }
+        std::vector<int64_t> v_axes;
+        for (int64_t i = 0; i < input_dim_num; ++i) {
+            v_axes.push_back(i);
+        }
+        int num = v_axes.size();
+        std::vector<int64_t> dims = {};
+        if (num != 0) {
+            dims.push_back(num);
+        }
+        axes = Vec2Tensor(v_axes, dims, ge::DT_INT64);
+    }
+
     auto data1 = op::Const((ori_name + "_data1").c_str()).set_attr_value(axes);
     auto reducemax = op::ReduceMax((ori_name + "_ReduceMax").c_str()).set_input_x(data0).set_input_axes(data1);
 
@@ -90,21 +126,138 @@ static Status ParseOpToGraphReduceMax(const ge::Operator& op, Graph& graph)
     return SUCCESS;
 }
 
+static Status ParseParamsReduceMax13(const Message* op_src, ge::Operator& op_dest)
+{
+    const NodeProto* node = dynamic_cast<const NodeProto*>(op_src);
+    if (node == nullptr) {
+        OP_LOGE("ReduceMax13", "Dynamic cast op_src to NodeProto failed.");
+        return FAILED;
+    }
+    op_dest.SetAttr("original_type", "ai.onnx::13::ReduceMax");
+
+    int input_size = node->input_size();
+    bool keep_dims = true;
+    int noop_with_empty_axes = 0;
+    for (const auto& attr : node->attribute()) {
+        if (attr.name() == "keepdims" && attr.type() == ge::onnx::AttributeProto::INT) {
+            keep_dims = (attr.i() == 1);
+        } else if (attr.name() == "noop_with_empty_axes" && attr.type() == ge::onnx::AttributeProto::INT) {
+            noop_with_empty_axes = attr.i();
+        }
+    }
+
+    op_dest.SetAttr("name", node->name());
+    op_dest.SetAttr("input_size", input_size);
+    op_dest.SetAttr("keep_dims", keep_dims);
+    op_dest.SetAttr("noop_with_empty_axes", noop_with_empty_axes);
+    return SUCCESS;
+}
+
+namespace {
+struct ReduceMax13Prop {
+    std::string ori_name;
+    bool keep_dims = false;
+    int input_num = 1;
+    int empty_axes = 0;
+};
+
+Status GetProperty(const Operator& op, ReduceMax13Prop& prop)
+{
+    if (op.GetAttr("name", prop.ori_name) != SUCCESS) {
+        OP_LOGE(GetOpName(op).c_str(), "get name from op failed.");
+        return FAILED;
+    }
+
+    if (op.GetAttr("keep_dims", prop.keep_dims) != SUCCESS) {
+        OP_LOGE(GetOpName(op).c_str(), "get keep_dims from op failed");
+        return FAILED;
+    }
+
+    if (op.GetAttr("input_size", prop.input_num) != SUCCESS) {
+        OP_LOGE(GetOpName(op).c_str(), "get input_num from op failed");
+        return FAILED;
+    }
+
+    if (op.GetAttr("noop_with_empty_axes", prop.empty_axes) != SUCCESS) {
+        OP_LOGE(GetOpName(op).c_str(), "get attribute noop_with_empty_axes failed");
+        return FAILED;
+    }
+    return SUCCESS;
+}
+
+} // namespace
+
+static Status ParseOpToGraphReduceMax13(const Operator& op, Graph& graph)
+{
+    ReduceMax13Prop prop;
+    if (GetProperty(op, prop) != SUCCESS) {
+        return FAILED;
+    }
+    auto data0 = op::Data((prop.ori_name + "_data0").c_str()).set_attr_index(0);
+    int num_input = 2;
+    if (prop.input_num == 1 && prop.empty_axes == 0) {
+        int64_t input_dim_num = 0;
+        if (GetInputTensorDimNum(op, input_dim_num) != SUCCESS) {
+            OP_LOGE(GetOpName(op).c_str(), "Failed to get input tensor dimensions");
+            return FAILED;
+        }
+
+        std::vector<int64_t> v_axes;
+        for (int64_t i = 0; i < input_dim_num; ++i) {
+            v_axes.push_back(i);
+        }
+        ge::TensorDesc tensorDesc;
+        std::vector<int64_t> dims = {input_dim_num};
+        ge::Shape shape(dims);
+        tensorDesc.SetShape(shape);
+        tensorDesc.SetDataType(DT_INT64);
+        ge::Tensor tensor(tensorDesc, reinterpret_cast<uint8_t*>(v_axes.data()), v_axes.size() * sizeof(int64_t));
+        auto axes = op::Const((prop.ori_name + "_axes").c_str()).set_attr_value(tensor);
+        std::vector<Operator> inputs{data0, axes};
+        std::vector<std::pair<Operator, std::vector<size_t> > > output_indexs;
+        auto reducemax = op::ReduceMax((prop.ori_name + "_ReduceMax").c_str())
+                             .set_input_x(data0)
+                             .set_input_axes(axes)
+                             .set_attr_keep_dims(prop.keep_dims)
+                             .set_attr_noop_with_empty_axes(prop.empty_axes);
+        output_indexs.emplace_back(reducemax, vector<std::size_t>{0});
+        graph.SetInputs(inputs).SetOutputs(output_indexs);
+    } else if (prop.input_num == num_input) {
+        auto data1 = op::Data((prop.ori_name + "_data1").c_str()).set_attr_index(1);
+        auto reducemax13 = op::ReduceMax((prop.ori_name + "_ReduceMax").c_str())
+                               .set_input_x(data0)
+                               .set_input_axes(data1)
+                               .set_attr_keep_dims(prop.keep_dims)
+                               .set_attr_noop_with_empty_axes(prop.empty_axes);
+        std::vector<Operator> inputs{data0, data1};
+        std::vector<std::pair<Operator, std::vector<size_t> > > output_indexs;
+        output_indexs.emplace_back(reducemax13, vector<std::size_t>{0});
+        graph.SetInputs(inputs).SetOutputs(output_indexs);
+    } else {
+        OP_LOGE(GetOpName(op).c_str(), "Input num or set attr is error");
+        return FAILED;
+    }
+    return SUCCESS;
+}
+
 // register ReduceMax op info to GE
 REGISTER_CUSTOM_OP("PartitionedCall")
     .FrameworkType(ONNX)
-    .OriginOpType({ge::AscendString("ai.onnx::8::ReduceMax"),
-                   ge::AscendString("ai.onnx::9::ReduceMax"),
-                   ge::AscendString("ai.onnx::10::ReduceMax"),
-                   ge::AscendString("ai.onnx::11::ReduceMax"),
-                   ge::AscendString("ai.onnx::12::ReduceMax"),
-                   ge::AscendString("ai.onnx::13::ReduceMax"),
-                   ge::AscendString("ai.onnx::14::ReduceMax"),
-                   ge::AscendString("ai.onnx::15::ReduceMax"),
-                   ge::AscendString("ai.onnx::16::ReduceMax"),
-                   ge::AscendString("ai.onnx::17::ReduceMax"),
-                   ge::AscendString("ai.onnx::18::ReduceMax")})
+    .OriginOpType(
+        {ge::AscendString("ai.onnx::8::ReduceMax"), ge::AscendString("ai.onnx::9::ReduceMax"),
+         ge::AscendString("ai.onnx::10::ReduceMax"), ge::AscendString("ai.onnx::11::ReduceMax"),
+         ge::AscendString("ai.onnx::12::ReduceMax")})
     .ParseParamsFn(ParseParamsReduceMax)
     .ParseOpToGraphFn(ParseOpToGraphReduceMax)
     .ImplyType(ImplyType::TVM);
-}  // namespace domi
+
+REGISTER_CUSTOM_OP("ReduceMax")
+    .FrameworkType(ONNX)
+    .OriginOpType(
+        {ge::AscendString("ai.onnx::13::ReduceMax"), ge::AscendString("ai.onnx::14::ReduceMax"),
+         ge::AscendString("ai.onnx::15::ReduceMax"), ge::AscendString("ai.onnx::16::ReduceMax"),
+         ge::AscendString("ai.onnx::17::ReduceMax"), ge::AscendString("ai.onnx::18::ReduceMax")})
+    .ParseParamsFn(ParseParamsReduceMax13)
+    .ParseOpToGraphFn(ParseOpToGraphReduceMax13)
+    .ImplyType(ImplyType::TVM);
+} // namespace domi
