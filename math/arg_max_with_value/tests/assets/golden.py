581b695bd70444aec3fa85eab573c8bcd8086923
h1234515 <huangzhiyuan21@huawei.com>
2026-06-18 16:35:54 +0800

    arg_max_v2，arg_max_with_value，arg_min，arg_min_with_value添加蓝区st
    Co-authored-by: h1234515<huangzhiyuan21@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3362 merge golden into master
    
    arg_max_v2，arg_max_with_value，arg_min，arg_min_with_value添加蓝区st
    
    Created-by: h1234515
    Commit-by: h1234515
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!3362

---
+++ math/arg_max_with_value/tests/assets/golden.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# ----------------------------------------------------------------------------
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# ----------------------------------------------------------------------------
+import numpy as np
+
+__golden__ = {
+    "kernel": {
+        "arg_max_with_value": "arg_max_with_value_golden"
+    }
+}
+
+
+def arg_max_with_value_golden(x, *, dimension, keep_dims=False, indice_dtype=None, **kwargs):
+    '''
+    Kernel golden for arg_max_with_value.
+    All the parameters follow @arg_max_with_value_def.cpp without outputs.
+    All the input Tensors are numpy.ndarray.
+    kwargs may contain: short_soc_version, input_ori_shapes, output_ori_shapes,
+        input_formats, output_formats, input_ori_formats, output_ori_formats,
+        input_dtypes, output_dtypes.
+    '''
+    ori_dtype = kwargs.get("input_dtypes", ["float32"])[0]
+    output_dtypes = kwargs.get("output_dtypes", ["int32", "float32"])
+    indice_dtype = output_dtypes[0] if indice_dtype is None else indice_dtype
+
+    x_bak = x
+    if "bfloat16" in str(ori_dtype).lower():
+        x = x.astype("float32")
+
+    axis = dimension % len(x.shape)
+    indices = np.argmax(x, axis=axis).astype(indice_dtype, copy=False)
+    values = np.take_along_axis(x_bak, np.expand_dims(indices, axis=axis), axis=axis)
+    return indices, values
