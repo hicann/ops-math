ec2a5cffc06f03ec3672ac04dcaa396f20fdef34
hw-zhangpanpan <zhangpanpan5@huawei.com>
2026-06-29 15:16:13 +0800

    unpack支持complex32
    Co-authored-by: hw-zhangpanpan<zhangpanpan5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3523 merge 9.1.0 into 9.1.0
    
    unpack支持complex32
    
    Created-by: hw-zhangpanpan
    Commit-by: hw-zhangpanpan
    Merged-by: cann-robot
    Description: ## 描述
    unpack算子原型定义和信息库不一致，原型定义支持complex32但是信息库不支持complex32，需要适配
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-math/issues/2037
    
    ## 测试
    kernel验证，冒烟测试
    
    ## 文档更新
    不涉及
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!3523

--- conversion/unpack/op_host/unpack_def.cpp
+++ conversion/unpack/op_host/unpack_def.cpp
@@ -19,19 +19,19 @@ class Unpack : public OpDef {
         this->Input("x")
             .ParamType(REQUIRED)
             .DataType({ge::DT_INT64, ge::DT_UINT64, ge::DT_INT32, ge::DT_UINT32, ge::DT_FLOAT, ge::DT_FLOAT16,
-                       ge::DT_INT8, ge::DT_UINT8, ge::DT_BF16, ge::DT_INT16, ge::DT_UINT16, ge::DT_BOOL})
+                       ge::DT_INT8, ge::DT_UINT8, ge::DT_BF16, ge::DT_INT16, ge::DT_UINT16, ge::DT_BOOL, ge::DT_COMPLEX32})
             .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
-                     ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+                     ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
             .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
-                                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+                                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
         this->Output("y")
             .ParamType(DYNAMIC)
             .DataType({ge::DT_INT64, ge::DT_UINT64, ge::DT_INT32, ge::DT_UINT32, ge::DT_FLOAT, ge::DT_FLOAT16,
-                       ge::DT_INT8, ge::DT_UINT8, ge::DT_BF16, ge::DT_INT16, ge::DT_UINT16, ge::DT_BOOL})
+                       ge::DT_INT8, ge::DT_UINT8, ge::DT_BF16, ge::DT_INT16, ge::DT_UINT16, ge::DT_BOOL, ge::DT_COMPLEX32})
             .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
-                     ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+                     ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
             .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND,
-                                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+                                ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
         this->Attr("num").AttrType(REQUIRED).Int();
         this->Attr("axis").AttrType(OPTIONAL).Int();
 
