7587512ae54533c9f4fbe452c32bb62dffd5aeac
jiangjiawei <jiangjiawei10@huawei.com>
2026-07-09 19:46:30 +0800

    修复real_div不同soc支持逻辑
    Co-authored-by: 蒋家炜<jiangjiawei10@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3880 merge real_div_bugfix into master
    
    修复real_div不同soc支持逻辑
    
    Created-by: jiangjiawei
    Commit-by: 蒋家炜
    Merged-by: cann-robot
    Description: ## 描述
    修复real_div支持逻辑
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-math/issues/2192
    
    ## 测试
    ![image.png](https://raw.gitcode.com/user-images/assets/7649531/e64f2b8e-81b9-4c83-99ad-5be45cb25bdd/image.png 'image.png')
    
    ## 文档更新
    不涉及
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!3880

--- math/real_div/op_api/realdiv.cpp
+++ math/real_div/op_api/realdiv.cpp
@@ -27,155 +27,160 @@ OP_TYPE_REGISTER(RealDiv);
 static const int MODE_REAL_DIV = 0;
 static const int MODE_TRUNC_DIV = 1;
 
-static const std::initializer_list<op::DataType> AICORE_DTYPE_SUPPORT_LIST = {op::DataType::DT_FLOAT,
-    op::DataType::DT_FLOAT16, op::DataType::DT_BF16, op::DataType::DT_BOOL};
+static const std::initializer_list<op::DataType> AICORE_DTYPE_SUPPORT_LIST = {
+    op::DataType::DT_FLOAT, op::DataType::DT_FLOAT16, op::DataType::DT_BF16, op::DataType::DT_BOOL};
 
-static const std::initializer_list<op::DataType> ASCEND910B_AICORE_DTYPE_SUPPORT_LIST = {op::DataType::DT_FLOAT,
-    op::DataType::DT_FLOAT16, op::DataType::DT_BF16, op::DataType::DT_BOOL, op::DataType::DT_INT32};
+static const std::initializer_list<op::DataType> ASCEND910B_AICORE_DTYPE_SUPPORT_LIST = {
+    op::DataType::DT_FLOAT, op::DataType::DT_FLOAT16, op::DataType::DT_BF16, op::DataType::DT_BOOL,
+    op::DataType::DT_INT32};
 
-static const std::initializer_list<op::DataType> ASCEND610LITE_DTYPE_SUPPORT_LIST = {op::DataType::DT_FLOAT,
-    op::DataType::DT_FLOAT16, op::DataType::DT_INT8, op::DataType::DT_UINT8, op::DataType::DT_INT32};
+static const std::initializer_list<op::DataType> ASCEND610LITE_DTYPE_SUPPORT_LIST = {
+    op::DataType::DT_FLOAT, op::DataType::DT_FLOAT16, op::DataType::DT_INT8, op::DataType::DT_UINT8,
+    op::DataType::DT_INT32};
 
-static const std::initializer_list<op::DataType> ASCEND950_DTYPE_SUPPORT_LIST = {op::DataType::DT_FLOAT,
-    op::DataType::DT_FLOAT16, op::DataType::DT_BF16, op::DataType::DT_BOOL, op::DataType::DT_INT32,
-    op::DataType::DT_INT64};
-
-// 判断当前芯片是否支持int32精度的RealDiv kernel（仅910B/910C支持）
-static bool IsInt32PrecisionSupported() {
-  auto socVersion = GetCurrentPlatformInfo().GetSocVersion();
-  return socVersion == SocVersion::ASCEND910B || socVersion == SocVersion::ASCEND910_93;
-}
+static const std::initializer_list<op::DataType> ASCEND950_DTYPE_SUPPORT_LIST = {
+    op::DataType::DT_FLOAT, op::DataType::DT_FLOAT16, op::DataType::DT_BF16,
+    op::DataType::DT_BOOL,  op::DataType::DT_INT32,   op::DataType::DT_INT64};
 
 // 根据芯片类型、dtype判断算子是否支持走aicore
-static bool IsAiCoreSupport(const aclTensor* self) {
-  // 根据dtype返回决定是否走aicore：true则走aicore
-  auto socVersion = GetCurrentPlatformInfo().GetSocVersion();
-  if (IsRegBase()) {
-    return CheckType(self->GetDataType(), ASCEND950_DTYPE_SUPPORT_LIST);
-  }
-  if (socVersion == SocVersion::ASCEND610LITE) {
-    return CheckType(self->GetDataType(), ASCEND610LITE_DTYPE_SUPPORT_LIST);
-  }
-  if (socVersion == SocVersion::ASCEND910B || socVersion == SocVersion::ASCEND910_93) {
-    return CheckType(self->GetDataType(), ASCEND910B_AICORE_DTYPE_SUPPORT_LIST);
-  }
-  return CheckType(self->GetDataType(), AICORE_DTYPE_SUPPORT_LIST);
+static bool IsAiCoreSupport(const aclTensor* self)
+{
+    // 根据dtype返回决定是否走aicore：true则走aicore
+    auto socVersion = GetCurrentPlatformInfo().GetSocVersion();
+    if (IsRegBase()) {
+        return CheckType(self->GetDataType(), ASCEND950_DTYPE_SUPPORT_LIST);
+    }
+    if (socVersion == SocVersion::ASCEND610LITE) {
+        return CheckType(self->GetDataType(), ASCEND610LITE_DTYPE_SUPPORT_LIST);
+    }
+    if (socVersion == SocVersion::ASCEND910B || socVersion == SocVersion::ASCEND910_93) {
+        return CheckType(self->GetDataType(), ASCEND910B_AICORE_DTYPE_SUPPORT_LIST);
+    }
+    return CheckType(self->GetDataType(), AICORE_DTYPE_SUPPORT_LIST);
 }
 
-bool IsRealDivSupportNonContiguous(const aclTensor* self) {
-  bool isSupportNonContiguous = IsRegBase();
-  return isSupportNonContiguous && IsAiCoreSupport(self);
+static bool is910BInt32Supported(const aclTensor* self, const aclTensor* other)
+{
+    auto socVersion = GetCurrentPlatformInfo().GetSocVersion();
+    return (socVersion == SocVersion::ASCEND910B || socVersion == SocVersion::ASCEND910_93) &&
+           self->GetDataType() == op::DataType::DT_INT32 && other->GetDataType() == op::DataType::DT_INT32;
+}
+
+bool IsRealDivSupportNonContiguous(const aclTensor* self)
+{
+    bool isSupportNonContiguous = IsRegBase();
+    return isSupportNonContiguous && IsAiCoreSupport(self);
 }
 
 // AICORE算子kernel
 static const aclTensor* RealDivAiCore(const aclTensor* self, const aclTensor* other, aclTensor* divOut,
-                                      aclOpExecutor* executor) {
-  L0_DFX(RealDivAiCore, self, other, divOut);
-  // 使用框架宏ADD_TO_LAUNCHER_LIST_AICORE，将AiCore RealDiv算子加入任务队列
-  // RealDiv是算子的OpType，self、other是算子的输入，divOut是算子的输出
-  ADD_TO_LAUNCHER_LIST_AICORE(RealDiv, OP_INPUT(self, other), OP_OUTPUT(divOut));
-  return divOut;
+                                      aclOpExecutor* executor)
+{
+    L0_DFX(RealDivAiCore, self, other, divOut);
+    // 使用框架宏ADD_TO_LAUNCHER_LIST_AICORE，将AiCore RealDiv算子加入任务队列
+    // RealDiv是算子的OpType，self、other是算子的输入，divOut是算子的输出
+    ADD_TO_LAUNCHER_LIST_AICORE(RealDiv, OP_INPUT(self, other), OP_OUTPUT(divOut));
+    return divOut;
 }
 
 // AICPU算子kernel
-static const aclTensor *RealDivAiCpu(const aclTensor *self, const aclTensor *other, aclTensor *divOut,
-                                      aclOpExecutor *executor) {
-  L0_DFX(RealDivAiCpu);
-  static internal::AicpuTaskSpace space("RealDiv");
-  auto ret = ADD_TO_LAUNCHER_LIST_AICPU(RealDiv, OP_ATTR_NAMES(), OP_INPUT(self, other), OP_OUTPUT(divOut));
-  CHECK_RET(ret == ACLNN_SUCCESS, nullptr);
-  return divOut;
+static const aclTensor* RealDivAiCpu(const aclTensor* self, const aclTensor* other, aclTensor* divOut,
+                                     aclOpExecutor* executor)
+{
+    L0_DFX(RealDivAiCpu);
+    static internal::AicpuTaskSpace space("RealDiv");
+    auto ret = ADD_TO_LAUNCHER_LIST_AICPU(RealDiv, OP_ATTR_NAMES(), OP_INPUT(self, other), OP_OUTPUT(divOut));
+    CHECK_RET(ret == ACLNN_SUCCESS, nullptr);
+    return divOut;
 }
 
-const aclTensor* RealDiv(const aclTensor* self, const aclTensor* other, aclOpExecutor* executor) {
-  op::Shape broadcastShape;
-  if (!BroadcastInferShape(self->GetViewShape(), other->GetViewShape(), broadcastShape)) {
-    OP_LOGE(ACLNN_ERR_PARAM_INVALID, "Broadcast %s and %s failed.", op::ToString(self->GetViewShape()).GetString(),
-            op::ToString(other->GetViewShape()).GetString());
-    return nullptr;
-  }
+const aclTensor* RealDiv(const aclTensor* self, const aclTensor* other, aclOpExecutor* executor)
+{
+    op::Shape broadcastShape;
+    if (!BroadcastInferShape(self->GetViewShape(), other->GetViewShape(), broadcastShape)) {
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID, "Broadcast %s and %s failed.", op::ToString(self->GetViewShape()).GetString(),
+                op::ToString(other->GetViewShape()).GetString());
+        return nullptr;
+    }
 
-  aclTensor* divOut;
-  if (self->GetDataType() == op::DataType::DT_BOOL ||
-      (self->GetDataType() == op::DataType::DT_INT32 && other->GetDataType() == op::DataType::DT_INT32 &&
-       IsInt32PrecisionSupported())) {
-    divOut = executor->AllocTensor(broadcastShape, op::DataType::DT_FLOAT);
-  } else {
-    divOut = executor->AllocTensor(broadcastShape, self->GetDataType());
-  }
+    aclTensor* divOut;
+    if (self->GetDataType() == op::DataType::DT_BOOL || is910BInt32Supported(self, other)) {
+        divOut = executor->AllocTensor(broadcastShape, op::DataType::DT_FLOAT);
+    } else {
+        divOut = executor->AllocTensor(broadcastShape, self->GetDataType());
+    }
 
-  if (IsAiCoreSupport(self)) {
-    return RealDivAiCore(self, other, divOut, executor);
-  } else {
-    return RealDivAiCpu(self, other, divOut, executor);
-  }
-  return divOut;
+    if (IsAiCoreSupport(self)) {
+        return RealDivAiCore(self, other, divOut, executor);
+    } else {
+        return RealDivAiCpu(self, other, divOut, executor);
+    }
+    return divOut;
 }
 
-const aclTensor* RealDiv(const aclTensor* self, const aclTensor* other, const int mode, aclOpExecutor* executor) {
-  op::Shape broadcastShape;
-  if (!BroadcastInferShape(self->GetViewShape(), other->GetViewShape(), broadcastShape)) {
-    OP_LOGE(ACLNN_ERR_PARAM_INVALID, "Broadcast %s and %s failed.", op::ToString(self->GetViewShape()).GetString(),
-            op::ToString(other->GetViewShape()).GetString());
-    return nullptr;
-  }
+const aclTensor* RealDiv(const aclTensor* self, const aclTensor* other, const int mode, aclOpExecutor* executor)
+{
+    op::Shape broadcastShape;
+    if (!BroadcastInferShape(self->GetViewShape(), other->GetViewShape(), broadcastShape)) {
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID, "Broadcast %s and %s failed.", op::ToString(self->GetViewShape()).GetString(),
+                op::ToString(other->GetViewShape()).GetString());
+        return nullptr;
+    }
 
-  bool isOutDtypeFloat = false;
-  if (mode == MODE_REAL_DIV &&
-      (self->GetDataType() == op::DataType::DT_BOOL ||
-       (self->GetDataType() == op::DataType::DT_INT32 && other->GetDataType() == op::DataType::DT_INT32 &&
-        IsInt32PrecisionSupported()))) {
-    isOutDtypeFloat = true;
-  }
+    bool isOutDtypeFloat = false;
+    if (mode == MODE_REAL_DIV &&
+        (self->GetDataType() == op::DataType::DT_BOOL || self->GetDataType() == op::DataType::DT_INT32)) {
+        isOutDtypeFloat = true;
+    }
 
-  if (mode == MODE_TRUNC_DIV && self->GetDataType() == op::DataType::DT_BOOL) {
-    isOutDtypeFloat = true;
-  }
+    if (mode == MODE_TRUNC_DIV && self->GetDataType() == op::DataType::DT_BOOL) {
+        isOutDtypeFloat = true;
+    }
 
-  aclTensor* divOut;
-  if (isOutDtypeFloat) {
-    divOut = executor->AllocTensor(broadcastShape, op::DataType::DT_FLOAT);
-  } else {
-    divOut = executor->AllocTensor(broadcastShape, self->GetDataType());
-  }
+    aclTensor* divOut;
+    if (isOutDtypeFloat) {
+        divOut = executor->AllocTensor(broadcastShape, op::DataType::DT_FLOAT);
+    } else {
+        divOut = executor->AllocTensor(broadcastShape, self->GetDataType());
+    }
 
-  if (IsAiCoreSupport(self)) {
-    return RealDivAiCore(self, other, divOut, executor);
-  } else {
-    return RealDivAiCpu(self, other, divOut, executor);
-  }
-  return divOut;
+    if (IsAiCoreSupport(self)) {
+        return RealDivAiCore(self, other, divOut, executor);
+    } else {
+        return RealDivAiCpu(self, other, divOut, executor);
+    }
+    return divOut;
 }
 
 static const aclTensor* RealDivKernel(const aclTensor* self, const aclTensor* other, aclTensor* divOut,
-                                      aclOpExecutor* executor) {
-  if (IsAiCoreSupport(self)) {
-    return RealDivAiCore(self, other, divOut, executor);
-  } else {
-    return RealDivAiCpu(self, other, divOut, executor);
-  }
+                                      aclOpExecutor* executor)
+{
+    if (IsAiCoreSupport(self)) {
+        return RealDivAiCore(self, other, divOut, executor);
+    } else {
+        return RealDivAiCpu(self, other, divOut, executor);
+    }
 }
 
-const aclTensor* RealDiv(const aclTensor* self, const aclTensor* other, bool isScalar, aclOpExecutor* executor) {
-  op::Shape broadcastShape;
-  if (!BroadcastInferShape(self->GetViewShape(), other->GetViewShape(), broadcastShape)) {
-    OP_LOGE(ACLNN_ERR_PARAM_INVALID, "Broadcast %s and %s failed.", op::ToString(self->GetViewShape()).GetString(),
-            op::ToString(other->GetViewShape()).GetString());
-    return nullptr;
-  }
+const aclTensor* RealDiv(const aclTensor* self, const aclTensor* other, bool isScalar, aclOpExecutor* executor)
+{
+    op::Shape broadcastShape;
+    if (!BroadcastInferShape(self->GetViewShape(), other->GetViewShape(), broadcastShape)) {
+        OP_LOGE(ACLNN_ERR_PARAM_INVALID, "Broadcast %s and %s failed.", op::ToString(self->GetViewShape()).GetString(),
+                op::ToString(other->GetViewShape()).GetString());
+        return nullptr;
+    }
 
-  aclTensor* divOut;
-  bool isInt32Precision = self->GetDataType() == op::DataType::DT_INT32 &&
-                          other->GetDataType() == op::DataType::DT_INT32 && IsInt32PrecisionSupported();
-  if ((isScalar && self->GetDataType() != op::DataType::DT_BOOL && !isInt32Precision) || 
-    ((!isScalar) && self->GetDataType() == other->GetDataType() && self->GetDataType() != op::DataType::DT_BOOL &&
-     !isInt32Precision)) {
-    divOut = executor->AllocTensor(broadcastShape, self->GetDataType());
-  } else {
-    divOut = executor->AllocTensor(broadcastShape, op::DataType::DT_FLOAT);
-  }
+    aclTensor* divOut;
 
-  return RealDivKernel(self, other, divOut, executor);
+    if ((isScalar && self->GetDataType() != op::DataType::DT_BOOL) ||
+        ((!isScalar) && self->GetDataType() == other->GetDataType() && self->GetDataType() != op::DataType::DT_BOOL)) {
+        divOut = executor->AllocTensor(broadcastShape, self->GetDataType());
+    } else {
+        divOut = executor->AllocTensor(broadcastShape, op::DataType::DT_FLOAT);
+    }
+
+    return RealDivKernel(self, other, divOut, executor);
 }
 
-}  // namespace l0op
+} // namespace l0op
