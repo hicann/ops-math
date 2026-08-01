a1c37b368d14c925e4a12a43dd8c3591f93cd298
wangqi_ai <wangqi195@hisilicon.com>
2026-08-01 17:53:54 +0800

    feat: 补充arg_min算子op_api UT
    Co-authored-by: wangqi_ai<wangqi195@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4046 merge feat/ut_arg_min into master
    
    feat: 补充arg_min算子op_api UT
    
    Created-by: wangqi_ai
    Commit-by: wangqi_ai
    Merged-by: cann-robot
    Description: ## 描述
    补充arg_min算子的op_api和op_host infershape级别单元测试，覆盖以下场景：
    - 各数据类型测试
    - 不同维度测试
    - 异常场景测试：nullptr输入输出、shape不匹配等
    
    ## 关联的Issue
    #2465
    ## 测试
    - 编译验证：bash build.sh -u --soc=ascend950 --ops=arg_min --noexec 编译通过
    
    ## 文档更新
    
    ## 类型标签
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：UT补充
    
    **关联 Issue:** #2465
    
    See merge request: cann/ops-math!4046

---
+++ math/arg_min/tests/ut/op_api/test_aclnn_arg_min.cpp
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include <array>
+#include <vector>
+#include "gtest/gtest.h"
+
+#include "../../../op_api/aclnn_argmin.h"
+
+#include "op_api_ut_common/op_api_ut.h"
+#include "op_api_ut_common/scalar_desc.h"
+#include "op_api_ut_common/tensor_desc.h"
+#include "opdev/platform.h"
+
+using namespace std;
+
+class l2_arg_min_test : public testing::Test {
+protected:
+    static void SetUpTestCase() { cout << "arg_min_test SetUp" << endl; }
+
+    static void TearDownTestCase() { cout << "arg_min_test TearDown" << endl; }
+};
+
+// test FLOAT with INT64 output
+TEST_F(l2_arg_min_test, case_arg_min_float_int64)
+{
+    auto self_tensor_desc = TensorDesc({3, 3, 3}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(-2.0, 2.0);
+    auto out_tensor_desc = TensorDesc({3, 3}, ACL_INT64, ACL_FORMAT_ND);
+
+    int64_t dim = 2;
+    bool keepdim = false;
+    auto ut = OP_API_UT(aclnnArgMin, INPUT(self_tensor_desc, dim, keepdim), OUTPUT(out_tensor_desc));
+    uint64_t workspace_size = 0;
+    aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+    EXPECT_EQ(aclRet, ACL_SUCCESS);
+}
+
+// test FLOAT16
+TEST_F(l2_arg_min_test, case_arg_min_float16)
+{
+    auto self_tensor_desc = TensorDesc({3, 3, 3}, ACL_FLOAT16, ACL_FORMAT_ND).ValueRange(-2.0, 2.0);
+    auto out_tensor_desc = TensorDesc({3, 3}, ACL_INT64, ACL_FORMAT_ND);
+
+    int64_t dim = 2;
+    bool keepdim = false;
+    auto ut = OP_API_UT(aclnnArgMin, INPUT(self_tensor_desc, dim, keepdim), OUTPUT(out_tensor_desc));
+    uint64_t workspace_size = 0;
+    aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+    EXPECT_EQ(aclRet, ACL_SUCCESS);
+}
+
+// test BF16
+TEST_F(l2_arg_min_test, case_arg_min_bf16)
+{
+    auto self_tensor_desc = TensorDesc({3, 3, 3}, ACL_BF16, ACL_FORMAT_ND).ValueRange(-2.0, 2.0);
+    auto out_tensor_desc = TensorDesc({3, 3}, ACL_INT64, ACL_FORMAT_ND);
+
+    int64_t dim = 2;
+    bool keepdim = false;
+    auto ut = OP_API_UT(aclnnArgMin, INPUT(self_tensor_desc, dim, keepdim), OUTPUT(out_tensor_desc));
+    uint64_t workspace_size = 0;
+    aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+    EXPECT_EQ(aclRet, ACL_SUCCESS);
+}
+
+// test INT32
+TEST_F(l2_arg_min_test, case_arg_min_int32)
+{
+    auto self_tensor_desc = TensorDesc({3, 3, 3}, ACL_INT32, ACL_FORMAT_ND).ValueRange(-10, 10);
+    auto out_tensor_desc = TensorDesc({3, 3}, ACL_INT64, ACL_FORMAT_ND);
+
+    int64_t dim = 2;
+    bool keepdim = false;
+    auto ut = OP_API_UT(aclnnArgMin, INPUT(self_tensor_desc, dim, keepdim), OUTPUT(out_tensor_desc));
+    uint64_t workspace_size = 0;
+    aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+    EXPECT_EQ(aclRet, ACL_SUCCESS);
+}
+
+// test keepdim=true
+TEST_F(l2_arg_min_test, case_arg_min_keepdim)
+{
+    auto self_tensor_desc = TensorDesc({3, 3, 3}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(-2.0, 2.0);
+    auto out_tensor_desc = TensorDesc({3, 3, 1}, ACL_INT64, ACL_FORMAT_ND);
+
+    int64_t dim = 2;
+    bool keepdim = true;
+    auto ut = OP_API_UT(aclnnArgMin, INPUT(self_tensor_desc, dim, keepdim), OUTPUT(out_tensor_desc));
+    uint64_t workspace_size = 0;
+    aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+    EXPECT_EQ(aclRet, ACL_SUCCESS);
+}
+
+// test 1d tensor
+TEST_F(l2_arg_min_test, case_arg_min_1d)
+{
+    auto self_tensor_desc = TensorDesc({10}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(-2.0, 2.0);
+    auto out_tensor_desc = TensorDesc({}, ACL_INT64, ACL_FORMAT_ND);
+
+    int64_t dim = 0;
+    bool keepdim = false;
+    auto ut = OP_API_UT(aclnnArgMin, INPUT(self_tensor_desc, dim, keepdim), OUTPUT(out_tensor_desc));
+    uint64_t workspace_size = 0;
+    aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+    EXPECT_EQ(aclRet, ACL_SUCCESS);
+}
+
+// test nullptr
+TEST_F(l2_arg_min_test, case_arg_min_nullptr)
+{
+    int64_t dim = 0;
+    bool keepdim = false;
+    auto ut = OP_API_UT(aclnnArgMin, INPUT((aclTensor*)nullptr, dim, keepdim), OUTPUT((aclTensor*)nullptr));
+    uint64_t workspace_size = 0;
+    aclnnStatus aclRet = ut.TestGetWorkspaceSize(&workspace_size);
+    EXPECT_EQ(aclRet, ACLNN_ERR_PARAM_NULLPTR);
+}
