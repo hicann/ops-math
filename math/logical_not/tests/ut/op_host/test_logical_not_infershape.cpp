564d0ffa8087552c4b78937236a6066be2de8f98
wangqi_ai <wangqi195@hisilicon.com>
2026-08-01 18:20:16 +0800

    feat: 补充logical_not算子op_host infershape UT
    Co-authored-by: wangqi_ai<wangqi195@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4069 merge feat/ut_logical_not into master
    
    feat: 补充logical_not算子op_host infershape UT
    
    Created-by: wangqi_ai
    Commit-by: wangqi_ai
    Merged-by: cann-robot
    Description: ## 描述
    补充logical_not算子的op_api和op_host infershape级别单元测试，覆盖以下场景：
    - 各数据类型测试
    - 不同维度测试
    - 异常场景测试：nullptr输入输出、shape不匹配等
    
    ## 关联的Issue
    #2465
    ## 测试
    - 编译验证：bash build.sh -u --soc=ascend950 --ops=logical_not --noexec 编译通过
    
    ## 文档更新
    
    ## 类型标签
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：UT补充
    
    **关联 Issue:** #2465
    
    See merge request: cann/ops-math!4069

---
+++ math/logical_not/tests/ut/op_host/test_logical_not_infershape.cpp
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+/*!
+ * \file test_logical_not_infershape.cpp
+ * \brief
+ */
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include "infershape_context_faker.h"
+#include "infershape_case_executor.h"
+
+class LogicalNotInfershape : public testing::Test {
+protected:
+    static void SetUpTestCase() { std::cout << "LogicalNotInfershape SetUp" << std::endl; }
+
+    static void TearDownTestCase() { std::cout << "LogicalNotInfershape TearDown" << std::endl; }
+};
+
+// LogicalNot is element-wise: output shape equals input shape.
+TEST_F(LogicalNotInfershape, logical_not_infershape_bool_test)
+{
+    gert::InfershapeContextPara infershapeContextPara("LogicalNot",
+                                                      {
+                                                          {{{2, 3, 4}, {2, 3, 4}}, ge::DT_BOOL, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                          {{{}, {}}, ge::DT_BOOL, ge::FORMAT_ND},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {
+        {2, 3, 4},
+    };
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+TEST_F(LogicalNotInfershape, logical_not_infershape_float_test)
+{
+    gert::InfershapeContextPara infershapeContextPara("LogicalNot",
+                                                      {
+                                                          {{{2, 3, 4}, {2, 3, 4}}, ge::DT_FLOAT, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                          {{{}, {}}, ge::DT_BOOL, ge::FORMAT_ND},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {
+        {2, 3, 4},
+    };
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+TEST_F(LogicalNotInfershape, logical_not_infershape_float16_test)
+{
+    gert::InfershapeContextPara infershapeContextPara("LogicalNot",
+                                                      {
+                                                          {{{4, 3, 4}, {4, 3, 4}}, ge::DT_FLOAT16, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                          {{{}, {}}, ge::DT_BOOL, ge::FORMAT_ND},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {
+        {4, 3, 4},
+    };
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+TEST_F(LogicalNotInfershape, logical_not_infershape_int32_test)
+{
+    gert::InfershapeContextPara infershapeContextPara("LogicalNot",
+                                                      {
+                                                          {{{2, 3, 4}, {2, 3, 4}}, ge::DT_INT32, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                          {{{}, {}}, ge::DT_BOOL, ge::FORMAT_ND},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {
+        {2, 3, 4},
+    };
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+TEST_F(LogicalNotInfershape, logical_not_infershape_bf16_test)
+{
+    gert::InfershapeContextPara infershapeContextPara("LogicalNot",
+                                                      {
+                                                          {{{2, 3, 4}, {2, 3, 4}}, ge::DT_BF16, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                          {{{}, {}}, ge::DT_BOOL, ge::FORMAT_ND},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {
+        {2, 3, 4},
+    };
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+TEST_F(LogicalNotInfershape, logical_not_infershape_1d_tensor_test)
+{
+    gert::InfershapeContextPara infershapeContextPara("LogicalNot",
+                                                      {
+                                                          {{{10}, {10}}, ge::DT_BOOL, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                          {{{}, {}}, ge::DT_BOOL, ge::FORMAT_ND},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {
+        {10},
+    };
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+TEST_F(LogicalNotInfershape, logical_not_infershape_5d_tensor_test)
+{
+    gert::InfershapeContextPara infershapeContextPara(
+        "LogicalNot",
+        {
+            {{{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}}, ge::DT_BOOL, ge::FORMAT_ND},
+        },
+        {
+            {{{}, {}}, ge::DT_BOOL, ge::FORMAT_ND},
+        });
+    std::vector<std::vector<int64_t>> expectOutputShape = {
+        {1, 2, 3, 4, 5},
+    };
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
+
+TEST_F(LogicalNotInfershape, logical_not_infershape_empty_tensor_test)
+{
+    gert::InfershapeContextPara infershapeContextPara("LogicalNot",
+                                                      {
+                                                          {{{0, 3, 4}, {0, 3, 4}}, ge::DT_BOOL, ge::FORMAT_ND},
+                                                      },
+                                                      {
+                                                          {{{}, {}}, ge::DT_BOOL, ge::FORMAT_ND},
+                                                      });
+    std::vector<std::vector<int64_t>> expectOutputShape = {
+        {0, 3, 4},
+    };
+    ExecuteTestCase(infershapeContextPara, ge::GRAPH_SUCCESS, expectOutputShape);
+}
