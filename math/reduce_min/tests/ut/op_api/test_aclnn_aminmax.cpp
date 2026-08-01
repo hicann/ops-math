578c8e3988210107e20b617917d712d3c1c2a9b9
wangqi_ai <wangqi195@hisilicon.com>
2026-08-01 18:20:45 +0800

    feat: 补充reduce_min算子op_api UT
    Co-authored-by: wangqi_ai<wangqi195@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4075 merge feat/ut_reduce_min into master
    
    feat: 补充reduce_min算子op_api UT
    
    Created-by: wangqi_ai
    Commit-by: wangqi_ai
    Merged-by: cann-robot
    Description: ## 描述
    补充reduce_min算子的op_api和op_host infershape级别单元测试，覆盖以下场景：
    - 各数据类型测试
    - 不同维度测试
    - 异常场景测试：nullptr输入输出、shape不匹配等
    
    ## 关联的Issue
    #2465
    ## 测试
    - 编译验证：bash build.sh -u --soc=ascend950 --ops=reduce_min --noexec 编译通过
    
    ## 文档更新
    
    ## 类型标签
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：UT补充
    
    **关联 Issue:** #2465
    
    See merge request: cann/ops-math!4075

---
+++ math/reduce_min/tests/ut/op_api/test_aclnn_aminmax.cpp
@@ -0,0 +1,200 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "gtest/gtest.h"
+#include "../../../op_api/aclnn_aminmax.h"
+#include "op_api_ut_common/tensor_desc.h"
+#include "op_api_ut_common/op_api_ut.h"
+
+using namespace std;
+
+class l2_aminmax_test : public testing::Test {
+protected:
+    static void SetUpTestCase() { cout << "l2_aminmax_test SetUp" << endl; }
+
+    static void TearDownTestCase() { cout << "l2_aminmax_test TearDown" << endl; }
+};
+
+// self为空指针
+TEST_F(l2_aminmax_test, l2_aminmax_test_nullptr_self)
+{
+    auto minOutDesc = TensorDesc({1, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto maxOutDesc = TensorDesc({1, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto dim = IntArrayDesc(vector<int64_t>{0});
+    bool keepDim = true;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(nullptr, dim, keepDim), OUTPUT(minOutDesc, maxOutDesc));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_NE(getWorkspaceResult, ACLNN_ERR_INNER_NULLPTR);
+}
+
+// dim为空指针
+TEST_F(l2_aminmax_test, l2_aminmax_test_nullptr_dim)
+{
+    auto selfDesc = TensorDesc({2, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto minOutDesc = TensorDesc({1, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto maxOutDesc = TensorDesc({1, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    bool keepDim = true;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(selfDesc, nullptr, keepDim), OUTPUT(minOutDesc, maxOutDesc));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_NE(getWorkspaceResult, ACLNN_ERR_INNER_NULLPTR);
+}
+
+// minOut为空指针
+TEST_F(l2_aminmax_test, l2_aminmax_test_nullptr_minout)
+{
+    auto selfDesc = TensorDesc({2, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto maxOutDesc = TensorDesc({1, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto dim = IntArrayDesc(vector<int64_t>{0});
+    bool keepDim = true;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(selfDesc, dim, keepDim), OUTPUT(nullptr, maxOutDesc));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_NE(getWorkspaceResult, ACLNN_ERR_INNER_NULLPTR);
+}
+
+// maxOut为空指针
+TEST_F(l2_aminmax_test, l2_aminmax_test_nullptr_maxout)
+{
+    auto selfDesc = TensorDesc({2, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto minOutDesc = TensorDesc({1, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto dim = IntArrayDesc(vector<int64_t>{0});
+    bool keepDim = true;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(selfDesc, dim, keepDim), OUTPUT(minOutDesc, nullptr));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_NE(getWorkspaceResult, ACLNN_ERR_INNER_NULLPTR);
+}
+
+// 正常路径，float32
+TEST_F(l2_aminmax_test, l2_aminmax_test_dtype_float32)
+{
+    auto selfDesc = TensorDesc({2, 4}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(-2, 2);
+    auto minOutDesc = TensorDesc({1, 4}, ACL_FLOAT, ACL_FORMAT_ND).Precision(0.0001, 0.0001);
+    auto maxOutDesc = TensorDesc({1, 4}, ACL_FLOAT, ACL_FORMAT_ND).Precision(0.0001, 0.0001);
+    auto dim = IntArrayDesc(vector<int64_t>{0});
+    bool keepDim = true;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(selfDesc, dim, keepDim), OUTPUT(minOutDesc, maxOutDesc));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_EQ(getWorkspaceResult, ACL_SUCCESS);
+    // ut.TestPrecision();
+}
+
+// 正常路径，float16
+TEST_F(l2_aminmax_test, l2_aminmax_test_dtype_float16)
+{
+    auto selfDesc = TensorDesc({2, 4}, ACL_FLOAT16, ACL_FORMAT_ND).ValueRange(-2, 2);
+    auto minOutDesc = TensorDesc({1, 4}, ACL_FLOAT16, ACL_FORMAT_ND).Precision(0.001, 0.001);
+    auto maxOutDesc = TensorDesc({1, 4}, ACL_FLOAT16, ACL_FORMAT_ND).Precision(0.001, 0.001);
+    auto dim = IntArrayDesc(vector<int64_t>{0});
+    bool keepDim = true;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(selfDesc, dim, keepDim), OUTPUT(minOutDesc, maxOutDesc));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_EQ(getWorkspaceResult, ACL_SUCCESS);
+    // ut.TestPrecision();
+}
+
+// 正常路径，int64
+TEST_F(l2_aminmax_test, l2_aminmax_test_dtype_int64)
+{
+    auto selfDesc = TensorDesc({2, 4}, ACL_INT64, ACL_FORMAT_ND).ValueRange(-2, 2);
+    auto minOutDesc = TensorDesc({1, 4}, ACL_INT64, ACL_FORMAT_ND);
+    auto maxOutDesc = TensorDesc({1, 4}, ACL_INT64, ACL_FORMAT_ND);
+    auto dim = IntArrayDesc(vector<int64_t>{0});
+    bool keepDim = true;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(selfDesc, dim, keepDim), OUTPUT(minOutDesc, maxOutDesc));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_EQ(getWorkspaceResult, ACL_SUCCESS);
+    // ut.TestPrecision();
+}
+
+// 正常路径，dim为1
+TEST_F(l2_aminmax_test, l2_aminmax_test_dim_1)
+{
+    auto selfDesc = TensorDesc({2, 4}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(-2, 2);
+    auto minOutDesc = TensorDesc({2, 1}, ACL_FLOAT, ACL_FORMAT_ND).Precision(0.0001, 0.0001);
+    auto maxOutDesc = TensorDesc({2, 1}, ACL_FLOAT, ACL_FORMAT_ND).Precision(0.0001, 0.0001);
+    auto dim = IntArrayDesc(vector<int64_t>{1});
+    bool keepDim = true;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(selfDesc, dim, keepDim), OUTPUT(minOutDesc, maxOutDesc));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_EQ(getWorkspaceResult, ACL_SUCCESS);
+    // ut.TestPrecision();
+}
+
+// 正常路径，keepDim为false
+TEST_F(l2_aminmax_test, l2_aminmax_test_keepdim_false)
+{
+    auto selfDesc = TensorDesc({2, 4}, ACL_FLOAT, ACL_FORMAT_ND).ValueRange(-2, 2);
+    auto minOutDesc = TensorDesc({4}, ACL_FLOAT, ACL_FORMAT_ND).Precision(0.0001, 0.0001);
+    auto maxOutDesc = TensorDesc({4}, ACL_FLOAT, ACL_FORMAT_ND).Precision(0.0001, 0.0001);
+    auto dim = IntArrayDesc(vector<int64_t>{0});
+    bool keepDim = false;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(selfDesc, dim, keepDim), OUTPUT(minOutDesc, maxOutDesc));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_EQ(getWorkspaceResult, ACL_SUCCESS);
+    // ut.TestPrecision();
+}
+
+// self为空tensor
+TEST_F(l2_aminmax_test, l2_aminmax_test_empty_self)
+{
+    auto selfDesc = TensorDesc({2, 0}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto minOutDesc = TensorDesc({1, 0}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto maxOutDesc = TensorDesc({1, 0}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto dim = IntArrayDesc(vector<int64_t>{0});
+    bool keepDim = true;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(selfDesc, dim, keepDim), OUTPUT(minOutDesc, maxOutDesc));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_EQ(getWorkspaceResult, ACL_SUCCESS);
+}
+
+// 异常路径，dim值不在self范围内
+TEST_F(l2_aminmax_test, l2_aminmax_dim_invalid)
+{
+    auto selfDesc = TensorDesc({2, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto minOutDesc = TensorDesc({1, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto maxOutDesc = TensorDesc({1, 4}, ACL_FLOAT, ACL_FORMAT_ND);
+    auto dim = IntArrayDesc(vector<int64_t>{2});
+    bool keepDim = true;
+
+    auto ut = OP_API_UT(aclnnAminmax, INPUT(selfDesc, dim, keepDim), OUTPUT(minOutDesc, maxOutDesc));
+
+    uint64_t workspaceSize = 0;
+    aclnnStatus getWorkspaceResult = ut.TestGetWorkspaceSize(&workspaceSize);
+    EXPECT_EQ(getWorkspaceResult, ACLNN_ERR_PARAM_INVALID);
+}
