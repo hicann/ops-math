77ea6585daf8fdbd16c63556b674240c76378b4c
nextyale <yaochen15@huawei.com>
2026-08-21 09:52:50 +0800

    迁移开源TF插件--conversion
    Co-authored-by: nextyale<yaochen15@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4310 merge transfer-conversion into master
    
    迁移开源TF插件--conversion
    
    Created-by: nextyale
    Commit-by: nextyale
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：迁移TF插件
    
    
    
    
    See merge request: cann/ops-math!4310

---
+++ conversion/concat/framework/concat_tf_plugin.cpp
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "register/register.h"
+
+namespace domi {
+static Status AutoMappingByOpFnConcatD(const ge::Operator& op_src, ge::Operator& op)
+{
+    std::vector<DynamicInputOutputInfo> dynamic_info;
+    dynamic_info.emplace_back(kInput, "x", 1, "N", 1);
+    return AutoMappingByOpFnDynamic(op_src, op, dynamic_info);
+}
+
+REGISTER_CUSTOM_OP("Concat")
+    .FrameworkType(TENSORFLOW)
+    .OriginOpType("Concat")
+    .ParseParamsByOperatorFn(AutoMappingByOpFnConcatD)
+    .ImplyType(ImplyType::TVM);
+} // namespace domi
