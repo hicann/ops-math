74d926610cd1bbe84fc23c963a9e08b00fe97c0c
gcw_DS4cmz2b <1312925094@qq.com>
2026-08-25 09:40:32 +0800

    SpaceToBatchND算子支持double数据类型编译
    Co-authored-by: Shichenyu<1312925094@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4923 merge fix/space_to_batch_nd_support_double into master
    
    SpaceToBatchND算子支持double数据类型编译
    
    Created-by: gcw_DS4cmz2b
    Commit-by: Shichenyu
    Merged-by: cann-robot
    Description: ## 描述
    使SpaceToBatchND算子符合设计原型支持的double数据类型
    ## 关联的Issue
    [https://gitcode.com/cann/ops-math/issues/2808]（space_to_batch_nd算子不支持double输入类型）
    ## 测试
    已通过ttk double类型用例测试，冒烟通过。
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-math!4923

--- conversion/space_to_batch_nd/op_kernel/space_to_batch_nd_apt.cpp
+++ conversion/space_to_batch_nd/op_kernel/space_to_batch_nd_apt.cpp
@@ -13,14 +13,34 @@
 #include "arch35/space_to_batch_nd_tiling_key.h"
 
 template <int TilingKey>
+__aicore__ inline void SpaceToBatchNDDispatch(GM_ADDR x, GM_ADDR y, GM_ADDR tiling)
+{
+    GET_TILING_DATA_WITH_STRUCT(SpaceToBatchNDTilingData, tilingData, tiling);
+
+    if constexpr (sizeof(DTYPE_X) == sizeof(int8_t)) {
+        SpaceToBatchND<int8_t, TilingKey> op;
+        op.Init(x, y, &tilingData);
+        op.Process();
+    } else if constexpr (sizeof(DTYPE_X) == sizeof(int16_t)) {
+        SpaceToBatchND<int16_t, TilingKey> op;
+        op.Init(x, y, &tilingData);
+        op.Process();
+    } else if constexpr (sizeof(DTYPE_X) == sizeof(int32_t)) {
+        SpaceToBatchND<int32_t, TilingKey> op;
+        op.Init(x, y, &tilingData);
+        op.Process();
+    } else if constexpr (sizeof(DTYPE_X) == sizeof(int64_t)) {
+        SpaceToBatchND<int64_t, TilingKey> op;
+        op.Init(x, y, &tilingData);
+        op.Process();
+    }
+}
+
+template <int TilingKey>
 __global__ __aicore__ void space_to_batch_nd(GM_ADDR x, GM_ADDR block_shape, GM_ADDR paddings, GM_ADDR y,
                                              GM_ADDR workspace, GM_ADDR tiling)
 {
     KERNEL_TASK_TYPE_DEFAULT(KERNEL_TYPE_AIV_ONLY);
     REGISTER_TILING_DEFAULT(SpaceToBatchNDTilingData);
-    GET_TILING_DATA_WITH_STRUCT(SpaceToBatchNDTilingData, tilingData, tiling);
-
-    SpaceToBatchND<DTYPE_X, TilingKey> op;
-    op.Init(x, y, &tilingData);
-    op.Process();
+    SpaceToBatchNDDispatch<TilingKey>(x, y, tiling);
 }
